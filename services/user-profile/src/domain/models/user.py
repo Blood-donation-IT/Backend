7c4edb38862a7ec94e033702cfab2742c9a97bf4
Makar <byreymakar@gmail.com>
2025-07-21 20:25:08 +0300

    refactor types

--- services/user-profile/src/domain/models/user.py
+++ services/user-profile/src/domain/models/user.py
@@ -1,9 +1,9 @@
 
 class User:
-    def __init__(self, user_id: str, name: str, email: str):
-        self.user_id = user_id
-        self.name = name
-        self.email = email
+    def __init__(self, user_id: int, name: str, email: str):
+        self.user_id:int = user_id
+        self.name:str = name
+        self.email:str = email
         #??
 
    
