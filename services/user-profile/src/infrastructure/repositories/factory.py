e31cb97d3c07b2d21ff33c3029d0464aabae98f3
Makar <byreymakar@gmail.com>
2025-09-03 22:52:36 +0300

    Add async context manager for user repository factory

---
+++ services/user-profile/src/infrastructure/repositories/factory.py
@@ -0,0 +1,10 @@
+from contextlib import asynccontextmanager
+from src.infrastructure.db.base import get_session
+from src.infrastructure.repositories.sqlalchemy_user_repository import SQlAlchemyUserRepository
+
+@asynccontextmanager
+async def get_user_repo():
+    """async fabric for user repository"""
+    async with get_session() as session:
+        repo = SQlAlchemyUserRepository(session=session)
+        yield repo
