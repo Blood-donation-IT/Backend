1a19951226e4d5d0417709928b6512bc244944f2
Danya4kas <daniilmosiychuk007@gmail.com>
2025-09-19 19:11:30 +0300

    add snowflake_generator

---
+++ services/application-management/src/infrastructure/id_generator/snowflake_generator.py
@@ -0,0 +1,16 @@
+from src.domain.services.i_id_generator import IIDGenerator
+from snowflake import SnowflakeGenerator
+class SnowflakeIDGenerator(IIDGenerator):
+    def __init__(self,
+                 instance:int,
+                 timestamp:int=946684800000, # 2000-01-01 00:00:00 UTC in milliseconds
+                 epoch:int=0,
+                 seq:int=0,
+                 ):
+        self._generator:SnowflakeGenerator = SnowflakeGenerator(instance=instance,
+                                                                timestamp=timestamp,
+                                                                epoch=epoch,
+                                                                seq=seq)
+    def generate(self) -> int:
+        return next(self._generator)
+    
