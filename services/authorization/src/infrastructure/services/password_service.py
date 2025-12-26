507c148255332cfdd9de1b358c431e0118fa4862
Danya4kas <daniilmosiychuk007@gmail.com>
2025-12-26 17:27:25 +0200

    create password_service hash_password bcrypt

---
+++ services/authorization/src/infrastructure/services/password_service.py
@@ -0,0 +1,12 @@
+import bcrypt
+
+class PasswordService:
+    @staticmethod
+    def hash_password(password: str) -> str:
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed.decode('utf-8')
+
+    @staticmethod
+    def verify_password(password: str, password_hash: str) -> bool:
+        return bcrypt.checkpw(password.encode('utf-8'), password_hash.encode('utf-8'))
