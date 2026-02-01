e68b47dded1226b747e625132eb579ee65a56630
Danya4kas <daniilmosiychuk007@gmail.com>
2026-02-01 23:13:17 +0200

    Refactor user_profile_model fields and directory path

--- services/admin-dashboard/src/models/authorization_model.py
+++ services/admin-dashboard/src/models/authorization_model.py
@@ -11,7 +11,6 @@ class AuthorizationORM(Base):
     email = Column(String, unique=True, nullable=False, index=True)
     password_hash = Column(String, nullable=False)
     name = Column(String, nullable=False)
-    birth_date = Column(DateTime(timezone=True), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
--- services/admin-dashboard/src/models/user_profile_model.py
+++ services/admin-dashboard/src/models/user_profile_model.py
@@ -4,13 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class UserORM(Base):
     __tablename__ = "users_"
-    __table_args__ = {'schema': None}  
+    __table_args__ = {'schema': None}
 
     id = Column(BigInteger, primary_key=True, autoincrement=False)
     email = Column(String, unique=True, nullable=False)
-    full_name = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     phone = Column(String, unique=True, nullable=True)
     blood_type = Column(String, nullable=True)
     is_verified = Column(Boolean, default=False, nullable=False)
@@ -22,6 +23,12 @@ class UserORM(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     password_hash = Column(String, nullable=True)
+    avatar_url = Column(String, nullable=True)
+    lives_saved_count = Column(Integer, default=0, nullable=False)
+    donor_status = Column(String, nullable=True)
+    has_donor_book = Column(Boolean, default=False, nullable=False)
+    test_is_done = Column(Boolean, default=False, nullable=False)
+    birth_date = Column(DateTime(timezone=True), nullable=True)
 
     def __repr__(self):
         return f'<UserORM(id={self.id}, email={self.email}, full_name={self.full_name})>'
